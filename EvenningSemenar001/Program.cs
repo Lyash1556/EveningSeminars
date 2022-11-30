168259b953e071eefd0bf2cfbc91719b3a4177e3
Aleksandr <72871900poker@gmail.com>
2022-11-30 22:27:07 +0300

    add exersize 001

---
+++ EvenningSemenar001/Program.cs
@@ -0,0 +1,12 @@
+﻿// семинар 1
+// задача 1
+
+Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+
+if(num1 == quad);
+
