5f8b69cc365fb0bd83abe620b03fe65f25905d3c
Lyash1556 <Lyash1556@gmail.com>
2022-12-01 15:51:22 +0300

    added exersize 2

--- EvenningSemenar001/Program.cs
+++ EvenningSemenar001/Program.cs
@@ -1,6 +1,7 @@
 ﻿// семинар 1
 // задача 1. Является ли первое число квадратом второго.
 
+/*
 Console.Write("input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("input a second number: ");
@@ -15,4 +16,18 @@ if(num1 == quad)
 else
 {
     Console.Write("No");
+}
+*/
+
+//Задача 2. Программа на вход принимает одно число N, а на выходе показывает все целые числа от -N до N.
+
+Console.Write("input an integer number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
 }
