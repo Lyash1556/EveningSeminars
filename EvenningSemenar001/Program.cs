a9d3995648a662b74f32688ff3c59e4746b13111
Lyash1556 <Lyash1556@gmail.com>
2022-12-01 15:36:19 +0300

    added exersize 1

--- EvenningSemenar001/Program.cs
+++ EvenningSemenar001/Program.cs
@@ -1,5 +1,5 @@
 ﻿// семинар 1
-// задача 1
+// задача 1. Является ли первое число квадратом второго.
 
 Console.Write("input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -8,5 +8,11 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 
 int quad = num2 * num2;
 
-if(num1 == quad);
-
+if(num1 == quad)
+{
+    Console.Write("yes");
+}
+else
+{
+    Console.Write("No");
+}
