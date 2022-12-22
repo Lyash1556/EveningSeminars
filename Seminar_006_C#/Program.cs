7a73ed6ffab0e7ca789705bc6908532b08d9ee50
Lyash1556 <Lyash1556@gmail.com>
2022-12-22 14:09:00 +0300

    add all tasks of Seminar 006

--- Seminar_006_C#/Program.cs
+++ Seminar_006_C#/Program.cs
@@ -1,4 +1,5 @@
 ﻿// ПЕРЕВОРОТ МАССИВА
+
 /*
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
@@ -50,7 +51,8 @@ ShowArray(myArray);
 
 
 
-//Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+//ЗАДАЧА////////////////Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
 
 /*
 bool FindThreeangle(int a, int b, int c)
@@ -71,8 +73,111 @@ Console.WriteLine(result);
 */
 
 
-//Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+//ЗАДАЧА////////////Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+            array[i] = new Random().Next(minValue, maxValue - 1);
+    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == array.Length - 1)
+            Console.Write(array[i]);
+        else
+            Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine();
+}
+
+int[] Fibonachi(int[] array, int first, int second)
+{
+    array[0] = first;
+    array[1] = second;
+
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 2] + array[i - 1]; 
+    }
+    return array;
+}
+
+Console.Write("Input namber elemets: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min pussible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max pussible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(n, min, max);
+
+ShowArray(myArray);
+
+Console.Write("Input first namber fibonachi: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second namber fibonachi: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Fibonachi(myArray, a, b);
+
+ShowArray(myArray);
+
+*/
 
 
+//ЗАДАЧА////////////////Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
 
-//Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+            array[i] = new Random().Next(minValue, maxValue - 1);
+    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == array.Length - 1)
+            Console.Write(array[i]);
+        else
+            Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine();
+}
+
+int[] CopyArray(int[] array, int[] newArray)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i]; 
+    }
+    return newArray;
+}
+
+Console.Write("Input namber elemets: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min pussible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max pussible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(n, min, max);
+int[] newArray = CreateRandomArray(n, min, max);
+
+ShowArray(myArray);
+
+ShowArray(newArray);
+
+CopyArray(myArray, newArray);
+
+ShowArray(newArray);
+*/
